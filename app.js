ecea4d300649533c2ed0182fb9c664652ec2e7a1
Afanasii Kurakin <afanasii@akura.co>
2015-06-27 10:47:11 +0000

    added stebeneva.ru#nodejs support, removed http proxy

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ var
   vhost = require('vhost'),
   app = express()
 
-var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
-
-
+process.env.HOME = '/home/ubuntu'
 _.each(['akura.co', 'afanasy.com', 'ysanafa.com', 'fanafan.us', 'fanafan.co', 'stebeneva.ru'], function (domain) {
-  if (domain == 'stebeneva.ru') {
-   app.use(vhost(domain, function (req, res) {
-     apiProxy.web(req, res, { target: 'http://localhost:8080' })
-   }))
-  }
-  else
-    app.use(vhost(domain, express().use(express.static(__dirname + '/' + domain))))  
+  if (domain == 'stebeneva.ru')
+    return app.use(vhost(domain, require(domain)))
+  app.use(vhost(domain, express().use(express.static(__dirname + '/' + domain))))  
 })
 
 app.listen(80)
