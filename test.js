f24699ab24f9fddd06caf6f3e0859f485363fc58
Raabb Ajam <raabbajam@gmail.com>
2015-10-17 11:18:34 +0700

    Separate akura.co specific files

--- test.js
+++ test.js
@@ -1,119 +1,24 @@
 var
-  expect = require('expect.js'),
-  supertest = require('supertest'),
-  Akura = require('./client'),
-  akura = new Akura({url: 'http://localhost:3000'}),
-  client = akura,
-  client2 = new Akura({url: 'http://kat.local:3000'}), // my local server
-  api = require('./api'),
-  server = supertest.agent(api)
+  app = require('./app'),
+  express = require('express')(),
+  request = require('supertest'),
+  server
 
-describe('Server', function () {
-  var expected = {
-    actions: { create: true, read: true, update: true, delete: true },
-    hosts: [ "akura.co", "afanasy.com", "ysanafa.com", "fanafan.us", "fanafan.co", "stebeneva.ru" ],
-    login: {
-      error: false,
-      message: 'Logging in successfully.',
-      token: 'test'
-    },
-    authorized: { message: 'hello, raabbajam', token: 'test' },
-    unauthorized: { message: 'hello, guest' }
-  }
+  express.use('/akura.co', app)
 
-  describe('using basic curl', function () {
-    it('returns action lists via request GET', function (done) {
-      server.
-        get('/action').
-        expect('Content-Type', /json/).
-        expect(200, expected.actions, done)
-    })
-    it('returns action lists via request POST', function (done) {
-      server.
-        post('/action').
-        expect('Content-Type', /json/).
-        expect(200, expected.actions, done)
-    })
-    it('returns host lists via request GET', function (done) {
-      server.
-        get('/hosts').
-        expect('Content-Type', /json/).
-        expect(200, expected.hosts , done)
-    })
-    it('access restricted page without logging in', function (done) {
-      server.
-        get('/home').
-        expect(200, expected.unauthorized, done)
-    })
-    it('access restricted page after successfully logging in', function (done) {
-      server.
-        post('/login').
-        send({username: 'raabbajam', password: 'test'}).
-        expect(200, expected.login, function () {
-          server.
-            get('/home').
-            expect(200, expected.authorized, done)
-        })
-    })
+describe('test mount', function () {
+  beforeEach(function () {
+    server = express.listen(express.get('port') || 3000)
   })
-  describe('using client module', function () {
-    var app
-    beforeEach(function () {
-      app = api.listen(api.get('port') || 3000)
-    })
-    afterEach(function (done) {
-      app.close(done)
-    })
-    it('returns action lists via `action` method', function (done) {
-      client.action(function (err, data) {
-        if (err) return done(err)
-        expect(data).to.eql(expected.actions)
-        done()
-      })
-    })
-    it('returns action lists via call method (test GET)', function (done) {
-      client.call('action', function (err, data) {
-        if (err) return done(err)
-        expect(data).to.eql(expected.actions)
-        done()
-      })
-    })
-    it('returns action lists via call method (test POST)', function (done) {
-      client.call({url: 'action', method: 'post'}, function (err, data) {
-        if (err) return done(err)
-        expect(data).to.eql(expected.actions)
-        done()
-      })
-    })
-    it('returns host lists via `hosts` method', function (done) {
-      client.hosts(function (err, hosts) {
-        if (err) return done(err)
-        expect(hosts).to.eql(expected.hosts)
-        done()
-      })
-    })
-    describe('test another server', function () {
-      it('returns action lists via `action` method to client2', function (done) {
-        client2.action(function (err, data) {
-          if (err) return done(err)
-          expect(data).to.eql(expected.actions)
-          done()
-        })
-      })
-    })
-    describe('add all available calls after calling `action`', function () {
-      it('returns action lists via `action` method and attaches available calls ' +
-        'to client', function (done) {
-        client.action(function (err, data) {
-          if (err) return done(err)
-          expect(data).to.eql(expected.actions)
-          expect(client.create).to.be.ok()
-          expect(client.read).to.be.ok()
-          expect(client.update).to.be.ok()
-          expect(client.delete).to.be.ok()
-          done()
-        })
-      })
-    })
+  afterEach(function (done) {
+    server.close(done)
+  })
+  it('mounted from other express app', function (done) {
+    request(server).
+      get('/akura.co/').
+      expect(function (res) {
+        console.log(res.text);
+      }).
+      expect(200, done)
   })
 })
