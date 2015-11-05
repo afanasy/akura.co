647d1ecab9556cadc0825bb8f131e4294fe90669
Afanasy Kurakin <afanasy@gmail.com>
2015-11-05 12:48:42 +0800

    added body parser

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var
   express = require('express'),
   hsts = require('hsts'),
+  bodyParser = require('body-parser'),
   quoteBot = require('quote-bot'),
   api = require('./api'),
   app = module.exports = express()
 
-app.
-  use(hsts({maxAge: 86400})).
-  use(express.static(__dirname + '/public')).
-  use('/telegram/quoteBot/hook', quoteBot()).
-  use(api)
+app.use(
+  hsts({maxAge: 86400}),
+  bodyParser.json(),
+  bodyParser.urlencoded({extended: true}),
+  express.static(__dirname + '/public'),
+  api
+)
+
+app.use('/telegram/quoteBot/hook', quoteBot())
