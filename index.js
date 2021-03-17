8836ebcf88249e3975f1365985fe85c8069642fe
Afanasii Kurakin <afanasy@gmail.com>
2021-03-17 12:58:00 +0300

    added bots setWebhook

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var _ = require('underscore')
 var express = require('express')
 var bodyParser = require('body-parser')
+var superagent = require('superagent')
 var config = require('./config')
+var setWebhook
 
 var app = module.exports = express().
   use(
@@ -12,9 +14,19 @@ var app = module.exports = express().
 
 _.each(config.bot, bot => {
   try {
-    app.use('/telegram/' + bot + '/hook', require(__dirname + '/../' + bot)())
+    var path = '/telegram/' + bot + '/hook'
+    if (setWebhook) {
+      var token = require(__dirname + '/../.' + bot).token
+      var hook = 'https://akura.co' + path
+      superagent.get('https://api.telegram.org/bot' + token + '/setWebhook').query({url: hook}).end((err, res) => {
+        if (err)
+          return console.log('setWebhook', hook, err)
+        console.log('setWebhook', hook, res.body)
+      })
+    }
+    app.use(path, require(__dirname + '/../' + bot)())
   }
   catch (e) {
-    console.log(e)
+    console.log(bot, e)
   }
 })
